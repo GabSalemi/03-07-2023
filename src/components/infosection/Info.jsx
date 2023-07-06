0bbf8a506726fb7b0fca55ed8ba10ab3ae6f2404
GabSalemi <salemi.gabriele94@gmail.com>
2023-07-06 22:45:59 +0200

    fix/style

--- src/components/infosection/Info.jsx
+++ src/components/infosection/Info.jsx
@@ -3,7 +3,7 @@ import "./info.scss";
 const Info = ( ) => {
     return <div className="info__section">
         <div className="vertical__title col-xs-2 col-sm-2 col-md-3 col-lg-4 col-xl-4">
-            <h2></h2>
+            <h2>Info Section</h2>
         </div>
         <div className="info__wrapper col-xs-10 col-sm-10 col-md-9 col-lg-8 col-log-8">
 
